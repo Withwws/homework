cd657dd0bf144725806c25a095a3c3627113f04b
Withwws <mondoi2003@gmail.com>
2022-09-02 22:22:37 +0700

    week5

---
+++ week5-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//วิธีที่2
+int main(){
+    int n,i=0;
+    printf("Enter numbers: ");
+    scanf("%d",&n);
+    while (i<n)
+    {
+        printf("*");
+        i++;
+    }
+    
+}
