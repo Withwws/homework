16373413c1207a5ae678d6a3541c5cce73425bcb
Withwws <mondoi2003@gmail.com>
2022-09-05 14:47:41 +0700

    edit1

--- week5-2.c
+++ week5-2.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 //วิธีที่2
+void drawstar(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    printf("*");
+    drawstar(n - 1);
+}
 int main(){
     int n,i=0;
     printf("Enter numbers: ");
     scanf("%d",&n);
-    while (i<n)
+    /*while (i<n)
     {
         printf("*");
         i++;
-    }
-    
+    }*/
+    drawstar(n);
 }
