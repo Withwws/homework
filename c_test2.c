8db4fe8258d7de4730ba69388ef41f9ace7a6598
Withwws <mondoi2003@gmail.com>
2022-10-05 15:20:56 +0700

    edi

--- c_test2.c
+++ c_test2.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 //วิธีที่ 2
 int main(){
     int a,b,c;
     printf("Enter 3 numbers: ");
     scanf("%d%d%d",&a,&b,&c);
     int num[] = {a,b,c};
-    int n = sizeof(num);
+    int n = sizeof(num)/ sizeof(num[0]);
     int min = num[0];
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < 3; i++) {
         if (num[i] < min) {
             min = num[i];
         }
     }
     printf("Min: %d", min);
+    return 0;
 }
