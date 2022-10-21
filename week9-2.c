cf1b33d165c05d1e7ae8450a294791c9deb820c8
Withwws <mondoi2003@gmail.com>
2022-10-21 20:29:14 +0700

    c'est terminer

---
+++ week9-2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+//วิธีที่2
+
+int main ()
+{
+    int a=0,b=0,j=0,k=0;
+    char word[100];
+    char up[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    char low[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    scanf("%s",word);
+       for (int i = 0; i < strlen(word); i++)
+    {
+        if (word[i] >= 'A' && word[i] <= 'Z')
+        {
+            up[j]=word[i];
+            j++;
+        }
+        if (word[i] >= 'a' && word[i] <= 'z')
+        {
+            low[k]=word[i];
+            k++;
+        }
+    }
+    printf("%s\n",word);
+    printf("%d\n%d",strlen(up),strlen(low));   
+}
